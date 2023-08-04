0c759767a783111b225d2d8e95d51ae852891372
jay.yang <858851768@qq.com>
2023-08-04 12:19:27 +0800

    🌈 style: code format by eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
+// https://juejin.cn/post/7226345144995758135
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "standard-with-typescript",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    parserOptions: {
+        ecmaVersion: 'latest', // 使用最新的 ECMAScript 版本
+        sourceType: 'module', // 表示校验整个项目是以esm模块的语法来进行校验的
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {},
+    env: {
+        browser: true, // 启用浏览器环境
+        es2021: true, // 使用 ES2021 版本的特性
+        commonjs: true, // 表示支持commonjs模块下的环境变量, 如果将这行去掉, 这个.eslint.js文件就会报错
+    },
+    parser: '@typescript-eslint/parser', // 使用 '@typescript-eslint/parser' 作为解析器，用于解析 TypeScript 代码
+    extends: [
+        'eslint:recommended', // 使用 ESLint 推荐的基本规则
+        'plugin:react/recommended', // 使用 react 插件推荐的规则
+        'plugin:@typescript-eslint/recommended', // 使用 @typescript-eslint 插件推荐的规则
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react'], // 启用 react 插件
+    rules: {
+        'prettier/prettier': ['error'], // 想在运行eslint时同时运行prettier，并且把prettier的错误作为eslint的错误来显示
+        'jsx-quotes': ['error', 'prefer-double'], // 在jsx的html元素中优先使用双引号
+    },
 };
