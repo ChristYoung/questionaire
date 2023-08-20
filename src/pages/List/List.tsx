cbe4917971f6453d4bd24db9960de1097ed1e568
jay.yang <858851768@qq.com>
2023-08-20 12:07:29 +0800

    ✨ feat: useReducer

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,5 +1,49 @@
-export interface ListProps extends React.HTMLAttributes<HTMLDivElement> {}
+import { useReducer, Reducer, useState, createContext, useContext } from 'react';
+import { reducer, initialState, TodoType, ActionType } from './List.store';
+import { nanoid } from 'nanoid';
 
-export const List: React.FC<ListProps> = (props: ListProps) => {
-    return <></>;
+const TodoContext = createContext({
+    state: initialState,
+    dispatch: (action: ActionType) => {},
+});
+
+export const List: React.FC<any> = (props: any) => {
+    const [state, dispatch] = useReducer<Reducer<TodoType[], ActionType>>(reducer, initialState);
+    return (
+        <TodoContext.Provider value={{ state, dispatch }}>
+            <InputForm></InputForm>
+            {state.map(s => (
+                <div key={s._id}>
+                    <span>
+                        {s.title}
+                        {s._id}
+                    </span>
+                    <button type="button" onClick={() => dispatch({ type: 'REMOVE_TODO', playload: s })}>
+                        x
+                    </button>
+                </div>
+            ))}
+        </TodoContext.Provider>
+    );
+};
+
+export const InputForm: React.FC<any> = (props: any) => {
+    const { state, dispatch } = useContext(TodoContext);
+    const [text, setText] = useState('');
+    const handleSubmit = () => {
+        if (!text.trim()) {
+            return;
+        }
+        const newTodo: TodoType = { _id: nanoid(5), title: text };
+        dispatch({ type: 'ADD_TODO', playload: newTodo });
+        setText('');
+    };
+    return (
+        <>
+            <input type="text" id="newTodo" onChange={e => setText(e.target.value)} />
+            <button type="button" onClick={() => handleSubmit()}>
+                ADD #{state.length + 1}
+            </button>
+        </>
+    );
 };
