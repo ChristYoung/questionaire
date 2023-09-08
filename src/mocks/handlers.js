47bba29ab25c057e5367b4b647f5e70dc9d29f6e
jay.yang <858851768@qq.com>
2023-09-08 14:00:18 +0800

    🐳 chore:

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,7 +2,8 @@ import { rest } from 'msw';
 import { questionnaireDetail } from '../enum/api.enum';
 
 export const handlers = [
-    rest.get(`*/${questionnaireDetail}`, (req, res, ctx) => {
+    // 根据id获取问卷中的问题列表
+    rest.get(`*/${questionnaireDetail}/:id`, (req, res, ctx) => {
         return res(ctx.status(200), ctx.json({}));
     }),
 ];
